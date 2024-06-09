41b531f5ab643a4d9079ba8082e183c10086d201
Lee9Bin <imagen33@daum.net>
2024-06-10 00:51:33 +0900

    feat : 인증 및 권한 부여 처리 정의

--- security0201/src/main/java/com/gyubs/security0201/config/ProjectConfig.java
+++ security0201/src/main/java/com/gyubs/security0201/config/ProjectConfig.java
@@ -2,11 +2,14 @@ package com.gyubs.security0201.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
@@ -26,5 +29,18 @@ public class ProjectConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    SecurityFilterChain configure(HttpSecurity http)
+            throws Exception {
+
+        http.httpBasic(Customizer.withDefaults());
+
+        http.authorizeHttpRequests(
+                c -> c.anyRequest().authenticated()
+        );
+
+        return http.build();
     }
 }
