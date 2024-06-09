da00861da894c01510ece17d16d07b451387ef62
Lee9Bin <imagen33@daum.net>
2024-06-10 01:11:01 +0900

    feat : 설정 정보 분리

--- security0201/src/main/java/com/gyubs/security0201/config/ProjectConfig.java
+++ security0201/src/main/java/com/gyubs/security0201/config/ProjectConfig.java
@@ -14,25 +14,49 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
-
-
+    //
+    //
+    // // @Bean
+    // // UserDetailsService userDetailsService() {
+    // //     // var user = User.withUsername("john")
+    // //     //         .password("12345")
+    // //     //         .authorities("read")
+    // //     //         .build();
+    // //     return new InMemoryUserDetailsManager(User.withUsername("gyub")
+    // //             .password("12345")
+    // //             .authorities("read")
+    // //             .build());
+    // // }
     // @Bean
-    // UserDetailsService userDetailsService() {
-    //     // var user = User.withUsername("john")
-    //     //         .password("12345")
-    //     //         .authorities("read")
-    //     //         .build();
-    //     return new InMemoryUserDetailsManager(User.withUsername("gyub")
-    //             .password("12345")
-    //             .authorities("read")
-    //             .build());
+    // public PasswordEncoder passwordEncoder() {
+    //     return NoOpPasswordEncoder.getInstance();
     // }
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
-
-
+    //
+    //
+    // // @Bean
+    // // SecurityFilterChain configure(HttpSecurity http)
+    // //         throws Exception {
+    // //
+    // //     http.httpBasic(Customizer.withDefaults());
+    // //     http.authorizeHttpRequests(
+    // //             c -> c.anyRequest().authenticated()
+    // //     );
+    // //
+    // //     http.userDetailsService(new InMemoryUserDetailsManager(User.withUsername("gyub")
+    // //             .password("12345")
+    // //             .authorities("read")
+    // //             .build()));
+    // //
+    // //     return http.build();
+    // // }
+    // private final CustomAuthenticationProvider authenticationProvider;
+    //
+    // public ProjectConfig(
+    //         CustomAuthenticationProvider authenticationProvider) {
+    //
+    //     this.authenticationProvider = authenticationProvider;
+    // }
+    //
     // @Bean
     // SecurityFilterChain configure(HttpSecurity http)
     //         throws Exception {
@@ -42,30 +66,6 @@ public class ProjectConfig {
     //             c -> c.anyRequest().authenticated()
     //     );
     //
-    //     http.userDetailsService(new InMemoryUserDetailsManager(User.withUsername("gyub")
-    //             .password("12345")
-    //             .authorities("read")
-    //             .build()));
-    //
     //     return http.build();
     // }
-    private final CustomAuthenticationProvider authenticationProvider;
-
-    public ProjectConfig(
-            CustomAuthenticationProvider authenticationProvider) {
-
-        this.authenticationProvider = authenticationProvider;
-    }
-
-    @Bean
-    SecurityFilterChain configure(HttpSecurity http)
-            throws Exception {
-
-        http.httpBasic(Customizer.withDefaults());
-        http.authorizeHttpRequests(
-                c -> c.anyRequest().authenticated()
-        );
-
-        return http.build();
-    }
 }
