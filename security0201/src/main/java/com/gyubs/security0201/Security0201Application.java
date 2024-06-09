24ab492ed1d5b4817f09b7081cc082296dfc4dff
Lee9Bin <imagen33@daum.net>
2024-06-10 00:00:17 +0900

    feat : setup security project

---
+++ security0201/src/main/java/com/gyubs/security0201/Security0201Application.java
@@ -0,0 +1,13 @@
+package com.gyubs.security0201;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Security0201Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Security0201Application.class, args);
+	}
+
+}
---
+++ security0201/src/test/java/com/gyubs/security0201/Security0201ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyubs.security0201;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Security0201ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
