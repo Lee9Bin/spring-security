99c2e5b9e189d2c9cab24577217fe6e245d97d38
Lee9Bin <imagen33@daum.net>
2024-06-11 20:49:18 +0900

    feat : 컨트롤러 구현

---
+++ udemy/springsecuritysection2/src/main/java/com/gyub/controller/LoansController.java
@@ -0,0 +1,14 @@
+package com.gyub.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoansController {
+
+    @GetMapping("/myLoans")
+    public String getLoanDetails() {
+        return "대출 내역 입니다.";
+    }
+
+}
