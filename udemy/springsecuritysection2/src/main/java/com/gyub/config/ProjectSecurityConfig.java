d8cb8f9a3f8176d7cc17bd5f13e500c2ea8357aa
Lee9Bin <imagen33@daum.net>
2024-06-11 21:22:54 +0900

    feat : 요구사항에 맞춘 설정 정보 구현

--- udemy/springsecuritysection2/src/main/java/com/gyub/config/ProjectSecurityConfig.java
+++ udemy/springsecuritysection2/src/main/java/com/gyub/config/ProjectSecurityConfig.java
@@ -1,22 +1,22 @@
 package com.gyub.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
-        http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
+        http.authorizeHttpRequests
+                        ((requests) -> requests
+                        .requestMatchers("/myAccount/**", "/myBalance", "/myLoans", "/myCards").authenticated()
+                        .requestMatchers("/notices", "/contact").permitAll())
+                        .formLogin(Customizer.withDefaults())
+                        .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 }
