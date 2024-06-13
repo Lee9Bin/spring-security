39ff467f6166b80c390de8acd0fa7eb8f35f3258
Lee9Bin <imagen33@daum.net>
2024-06-13 22:55:29 +0900

    feat : 사용자 지정 필터 클래스의 정의

---
+++ study/security9/src/main/java/com/laurentiuspilca/ssia/filters/CsrfTokenLogger.java
@@ -0,0 +1,28 @@
+package com.laurentiuspilca.ssia.filters;
+
+import org.springframework.security.web.csrf.CsrfToken;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class CsrfTokenLogger implements Filter {
+
+    private Logger logger =
+            Logger.getLogger(CsrfTokenLogger.class.getName());
+
+    @Override
+    public void doFilter(
+            ServletRequest request,
+            ServletResponse response,
+            FilterChain filterChain)
+            throws IOException, ServletException {
+
+        CsrfToken token =
+                (CsrfToken) request.getAttribute("_csrf");
+
+        logger.info("CSRF token " + token.getToken());
+
+        filterChain.doFilter(request, response);
+    }
+}
