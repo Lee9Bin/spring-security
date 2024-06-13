92982019a11fa54cadfe62128b8a41810a98b8f7
Lee9Bin <imagen33@daum.net>
2024-06-13 23:46:14 +0900

    feat : 스프링 보안에서 CSRF 보호가 작동하는 방식

--- study/security09/src/main/java/com/gyubs/config/ProjectConfig.java
+++ study/security09/src/main/java/com/gyubs/config/ProjectConfig.java
@@ -1,77 +1,31 @@
 package com.gyubs.config;
 
+import com.gyubs.filter.CsrfTokenLogger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CsrfFilter;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class ProjectConfig {
-    //
-    //
-    // // @Bean
-    // // UserDetailsService userDetailsService() {
-    // //     // var user = User.withUsername("john")
-    // //     //         .password("12345")
-    // //     //         .authorities("read")
-    // //     //         .build();
-    // //     return new InMemoryUserDetailsManager(User.withUsername("gyub")
-    // //             .password("12345")
-    // //             .authorities("read")
-    // //             .build());
-    // // }
-    // @Bean
-    // public PasswordEncoder passwordEncoder() {
-    //     return NoOpPasswordEncoder.getInstance();
-    // }
-    //
-    //
-    // // @Bean
-    // // SecurityFilterChain configure(HttpSecurity http)
-    // //         throws Exception {
-    // //
-    // //     http.httpBasic(Customizer.withDefaults());
-    // //     http.authorizeHttpRequests(
-    // //             c -> c.anyRequest().authenticated()
-    // //     );
-    // //
-    // //     http.userDetailsService(new InMemoryUserDetailsManager(User.withUsername("gyub")
-    // //             .password("12345")
-    // //             .authorities("read")
-    // //             .build()));
-    // //
-    // //     return http.build();
-    // // }
-    // private final CustomAuthenticationProvider authenticationProvider;
-    //
-    // public ProjectConfig(
-    //         CustomAuthenticationProvider authenticationProvider) {
-    //
-    //     this.authenticationProvider = authenticationProvider;
-    // }
-    //
-    // @Bean
-    // SecurityFilterChain configure(HttpSecurity http)
-    //         throws Exception {
-    //
-    //     http.httpBasic(Customizer.withDefaults());
-    //     http.authorizeHttpRequests(
-    //             c -> c.anyRequest().authenticated()
-    //     );
-    //
-    //     return http.build();
-    // }
-    @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
-    }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+    public SecurityFilterChain configure(HttpSecurity http)
+            throws Exception {
+
+        http.addFilterAfter(
+                        new CsrfTokenLogger(), CsrfFilter.class)
+                .authorizeHttpRequests(
+                        c -> c.anyRequest().permitAll()
+                );
+
+        return http.build();
     }
 }
