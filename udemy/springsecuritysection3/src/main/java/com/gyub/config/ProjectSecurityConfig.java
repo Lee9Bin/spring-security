e9a0022236dfae69ff2522e7616a0bd5071e4d0f
Lee9Bin <imagen33@daum.net>
2024-06-19 23:33:31 +0900

    feat : JdbcUserDetailsManager를 사용한 인증

--- udemy/springsecuritysection3/src/main/java/com/gyub/config/ProjectSecurityConfig.java
+++ udemy/springsecuritysection3/src/main/java/com/gyub/config/ProjectSecurityConfig.java
@@ -6,10 +6,14 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -25,39 +29,44 @@ public class ProjectSecurityConfig {
         return http.build();
     }
 
+    // @Bean
+    // public InMemoryUserDetailsManager userDetailsManager(){
+    //     /*
+    //      * withDefaultPasswordEncoder() 메서드를 사용하는 접근 방식 1
+    //      */
+    //     /*UserDetails admin = User.withDefaultPasswordEncoder()
+    //             .username("admin")
+    //             .password("12345")
+    //             .authorities("admin")
+    //             .build();
+    //
+    //     UserDetails user = User.withDefaultPasswordEncoder()
+    //             .username("user")
+    //             .password("12345")
+    //             .authorities("read")
+    //             .build();
+    //
+    //     return new InMemoryUserDetailsManager(admin, user);*/
+    //
+    //     /*
+    //     NoOpPasswordEncoder Bean을 사용하는 접근 방식 2
+    //      */
+    //     UserDetails admin = User.withUsername("admin")
+    //             .password("12345")
+    //             .authorities("admin")
+    //             .build();
+    //
+    //     UserDetails user = User.withUsername("user")
+    //             .password("12345")
+    //             .authorities("read")
+    //             .build();
+    //
+    //     return new InMemoryUserDetailsManager(admin, user);
+    // }
+
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-        /*
-         * withDefaultPasswordEncoder() 메서드를 사용하는 접근 방식 1
-         */
-        /*UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("12345")
-                .authorities("admin")
-                .build();
-
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("12345")
-                .authorities("read")
-                .build();
-
-        return new InMemoryUserDetailsManager(admin, user);*/
-
-        /*
-        NoOpPasswordEncoder Bean을 사용하는 접근 방식 2
-         */
-        UserDetails admin = User.withUsername("admin")
-                .password("12345")
-                .authorities("admin")
-                .build();
-
-        UserDetails user = User.withUsername("user")
-                .password("12345")
-                .authorities("read")
-                .build();
-
-        return new InMemoryUserDetailsManager(admin, user);
+    public UserDetailsService userDetailsService(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
